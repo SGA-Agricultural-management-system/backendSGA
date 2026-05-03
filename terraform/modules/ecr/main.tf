669ef6956bc61aeec94b6619f55da516e1d459cd
Alejandro <jean.pena,2022@upb.edu.co>
2026-05-02 23:51:05 -0500

    sync

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -10,26 +10,28 @@ resource "aws_ecr_repository" "repo" {
 resource "aws_ecr_lifecycle_policy" "cleanup" {
   repository = aws_ecr_repository.repo.name
   policy = jsonencode({
-    rules = [{
-      rulePriority = 1
-      description  = "Keep last 10 images, remove untagged"
-      selection = {
-        tagStatus   = "any"
-        countType   = "imageCountMoreThan"
-        countNumber = 10
+    rules = [
+      {
+        rulePriority = 1,
+        description  = "Remove images without tag after 1 day",
+        selection = {
+          tagStatus   = "untagged",
+          countType   = "sinceImagePushed",
+          countUnit   = "days",
+          countNumber = 1
+        },
+        action = { type = "expire" }
+      },
+      {
+        rulePriority = 2,
+        description  = "Keep last 10 images",
+        selection = {
+          tagStatus   = "any",
+          countType   = "imageCountMoreThan",
+          countNumber = 10
+        },
+        action = { type = "expire" }
       }
-      action = { type = "expire" }
-    },
-    {
-      rulePriority = 2
-      description  = "Remove images without tag after 1 day"
-      selection = {
-        tagStatus   = "untagged"
-        countType   = "sinceImagePushed"
-        countUnit   = "days"
-        countNumber = 1
-      }
-      action = { type = "expire" }
-    }]
+    ]
   })
 }
