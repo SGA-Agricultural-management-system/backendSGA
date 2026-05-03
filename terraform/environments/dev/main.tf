f617f22ee79384a79e5e64e759f6dd24ddf74ddf
Alejandro <jean.pena,2022@upb.edu.co>
2026-05-02 23:17:18 -0500

    sync

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -18,7 +18,12 @@ resource "aws_security_group" "ecs" {
     protocol        = "tcp"
     security_groups = [module.alb.alb_sg_id]
   }
-  egress { from_port = 0; to_port = 0; protocol = "-1"; cidr_blocks = ["0.0.0.0/0"] }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+}
 }
 
 resource "aws_security_group" "rds" {
